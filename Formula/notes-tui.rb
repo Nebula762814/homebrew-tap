ac0c3769259db910b657b51d5592c9ada5d6e44f
Riddick Burke <nebula@Riddicks-MacBook-Pro.local>
2026-04-02 12:13:31 -0700

    Fix sha256

--- Formula/notes-tui.rb
+++ Formula/notes-tui.rb
@@ -2,7 +2,7 @@ class NotesTui < Formula
   desc "Terminal-based notes app"
   homepage "https://github.com/Nebula762814/Notes-TUI"
   url "https://github.com/Nebula762814/Notes-TUI/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PASTE_SHA256_HERE"
+  sha256 "a22bdcb86e85a1c18f477b6e2eb347f97e67d9c4a6890d6d91cbdd436b60c5ce"
   license "MIT"
 
   depends_on "python@3.11"
